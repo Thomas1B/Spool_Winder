05d158bf4e62395aa630499cd1ff7c72522cd05b
Thomas Bourgeois <thomasbourgeois2@hotmail.com>
2024-06-25 10:56:00 -0700

    rewritten entire program

--- controls.py
+++ controls.py
@@ -3,28 +3,51 @@ This is a python script to run the 3d filament spool winder. It controls a Nema1
 a DVR8825 driver and a limit switch to detect the filament.
 '''
 
-import utime
+import utime  # type: ignore
 from machine import Pin  # type: ignore
 
 from DVR8825_Driver import Stepper  # type: ignore
 
-dir_pin = 4
-step_pin = 5
-enable_pin = 6
-
 
 if __name__ == "__main__":
-    try:
-        stepper = Stepper(step_pin=7, dir_pin=6, enable_pin=8,
-                          step_mode=2,
-                          mode_pins=(0, None, None))
 
-        stepper.set_speed(300)
+    print("Starting...")
+    limit_swt = Pin(12, Pin.IN)
+    btn = Pin(14, Pin.IN)
+    pico_led = Pin('LED', Pin.OUT)
+
+    led_flash_time = 500  # milliseconds
+
+    try:
+
+        stepper = Stepper(step_pin=1, dir_pin=0, enable_pin=2,
+                          mode_pins=(3, 4, 5))
+        stepper.set_step_mode(2)
+        stepper.set_speed(400)
+        stepper.set_direction(stepper.CCW)
+
         stepper.enable()
 
-        stepper.move_to_abs(200)
+        print("Press Button to start")
+        while not btn.value():
+            if btn.value():
+                utime.sleep(1)
+                break
 
+        last_flash = 0
+        print('Starting...')
+        while not limit_swt.value() or not btn.value():
+            if utime.ticks_ms() - last_flash >= led_flash_time:
+                last_flash = utime.ticks_ms()
+                pico_led.toggle()
+
+            stepper.one_step()
+
+        print("Finished!")
+        pico_led.off()
         stepper.disable()
 
     except KeyboardInterrupt:
         stepper.disable()
+        pico_led.off()
+        print("Force Stop")
